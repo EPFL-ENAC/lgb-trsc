d47b6856b96972b306b0f8e40fc47302d985a780
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-02-10 15:20:37 +0100

    feat: add new images and enhance content on AboutPage and TheRedSeaPage for improved information on coral reefs and conservation efforts #28 #2

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="about-page">
     <p class="top-element q-pa-md">
-     <a class="trsc-link" href="https://trsc.org" target="_blank">
-       The Transnational Red Sea
-      </a>
-      Center was created by the Swiss Federal Institute of Technology Lausanne (EPFL) and the King Abdullah University of Science and Technology (KAUST) in 2019. The TRSC is a research center that aims to study the Red Sea and its coral reefs. The center is located in Thuwal, Saudi Arabia.
+     <a class="trsc-link" href="https://trsc.org" target="_blank">The Transnational Red Sea</a>, established in 2019 at <a target="_blank" class="trsc-link" href="https://www.epfl.ch/">the Ecole Polytechnique Federale de Lausanne</a> with support from the Swiss Foreign Ministry, is an independent non-profit organisation dedicated to coral reef conservation in the Red Sea. By drawing on Switzerland's neutrality and scientific expertise, the Center promotes scientific collaboration among Red Sea countries to better understand how the region's corals have developed resilience to climate change. It also develops innovative, cost-effective technologies to enhance coral reef monitoring with higher resolution, standardisation, and scalability. The Center advocates for environmental protection policies by sharing these solutions with Red Sea nations and beyond, supporting science-based efforts to conserve coral reefs globally.
     </p>
     <div class="bottom-element">
       <div class="card" v-for="person in people" :key="person.id">
--- frontend/src/pages/TheRedSeaPage.vue
+++ frontend/src/pages/TheRedSeaPage.vue
@@ -3,7 +3,86 @@
     <!-- <h1>The Red Sea</h1> -->
     <p class="q-pa-md">
 
-      The Red Sea is a unique region of the world, hosting exceptionally resistant coral reefs in the face of climate change……
+      <p>
+        Coral reefs, vital ecosystems which support a third of all marine species,
+        are facing severe threats from climate change, such as rising sea
+        temperatures and ocean acidification, as well as local issues like
+        pollution and overfishing. The past decade has seen devastating coral
+        bleaching events, notably in the Great Barrier Reef, where half of its
+        coral cover has been lost. With current climate trends, only about 10%
+        of coral reefs are expected to survive by 2050, leading to a massive
+        loss in biodiversity and threatening the livelihoods of millions who
+        depend on reefs for food, tourism, and coastal protection.
+      </p>
+      <p>
+        However, the Red Sea offers hope. Studies, including those by teams at
+        EPFL, and researchers from Israel, Jordan, Saudi Arabia, and Egypt, show
+        that corals in the Gulf of Aqaba and northern Red Sea are more resilient
+        to rising temperatures compared to other reefs. These corals have evolved
+        in unique conditions, making them highly resistant to heat stress. This
+        offers a chance for the Red Sea reefs to survive and possibly act as a
+        source for restoring other reefs globally, but protection from further
+        environmental threats is crucial and urgent.
+      </p>
+      <p>
+        <b>The Red Sea and Gulf of Aden (RSGA)</b>
+      </p>
+      <p>
+        The Red Sea and Gulf of Aden (RSGA region) host about 13,605 km² of coral
+        reefs, representing 5.24% of the world’s total. This region is home to a
+        high diversity of marine life, including 401 species of corals (359 in
+        the Red Sea, with 6,4% of endemics, (Berumen et al, 2019,
+        https://link.springer.com/chapter/10.1007/978-3-030-05802-9_7)) and over
+        1,100 fish species, with 165 endemic to the Red Sea (Bogorodsky et
+        Randall, 2019,
+        https://link.springer.com/chapter/10.1007/978-3-319-99417-8_14). The Red
+        Sea’s warm (SST ranging from 27°C in the North to above 32 in the south),
+        saline waters (ranging from 42‰ in the north to 37‰ in the south), and
+        nutrient-poor environment support unique coral ecosystems, while the
+        eastern portion of the Gulf of Aden is influenced by upwelling of cooler,
+        nutrient-rich water, which tend to limit the develop of fringing coral
+        reefs. The region is vital for about 240 million of people who rely on
+        the health of the sea for fishing, tourism, and coastal resources.
+      </p>
+      <p>
+        However, local disturbances like pollution and coastal development
+        threaten reef health and resilience. Because many of these impacts cross
+        national borders, coordinated, regional action is needed to protect these
+        ecosystems. Currently, coral reef monitoring in the region is sparse,
+        inconsistent, and costly, making it difficult to compare data across
+        countries and implement an effective conservation policy, coherent at a
+        regional level.
+      </p>
+      <p>
+        <b>Our scientific objectives</b>
+      </p>
+      <ul>
+        <li>
+          Understanding Coral Resilience: The first goal is to understand why Red
+          Sea corals are so resilient to climate change, potentially offering
+          insights into how other reefs could adapt.
+        </li>
+        <li>
+          Innovative Monitoring Tools: The second goal is to develop affordable,
+          high-resolution monitoring tools that can be standardized across RSGA
+          countries, enabling better tracking of reef health and more coordinated
+          science-based conservation efforts.
+        </li>
+        <li>
+          Integrated Research Framework: The third goal is to create a
+          comprehensive, cost-effective, and scalable framework for assessing
+          reef vulnerability and guiding conservation, which can be applied
+          globally.
+        </li>
+      </ul>
+      <p>
+        The TRSC involves scientists from Red Sea countries in every step of the
+        research and transfers their innovative technologies through training
+        workshops, ensuring local expertise, leadership, and autonomy to support
+        long-term regional monitoring programs. By using advanced, affordable
+        technology developed at EPFL, the center fosters international
+        partnerships and trains the next generation of scientists in the region.
+      </p>
     </p>
   </div>
 </template>
