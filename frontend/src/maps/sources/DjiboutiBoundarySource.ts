64e623a36201b83188ed1856fc150059ad471fd1
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-02-12 09:23:30 +0100

    fix: remove unnecessary query parameter from Djibouti boundary source URL

--- frontend/src/maps/sources/DjiboutiBoundarySource.ts
+++ frontend/src/maps/sources/DjiboutiBoundarySource.ts
@@ -2,6 +2,6 @@ import { PMTilesVectorSource } from 'ol-pmtiles';
 
 export const createPMTilesSource = () =>
   new PMTilesVectorSource({
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/djibouti-20250115143859/boundary/boundary.pmtiles?ok=13',
+    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/djibouti-20250115143859/boundary/boundary.pmtiles',
     attributions: 'Your Attribution Here',
   });
