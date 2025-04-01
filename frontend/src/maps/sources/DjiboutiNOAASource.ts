02852e2ec123fac036ea37949a9d771c0dad7929
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-04-01 11:20:26 +0200

    feat: update tiffsUrls and metadata structure for improved data handling in DjiboutiNOAASource

--- frontend/src/maps/sources/DjiboutiNOAASource.ts
+++ frontend/src/maps/sources/DjiboutiNOAASource.ts
@@ -85,6 +85,25 @@ const legendVariables = {
   },
 };
 
+export const tiffsUrls = [
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/CHL_monthly_mean_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/CHL_monthly_mean_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/DHW_annual_max_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/DHW_annual_max_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SCV_monthly_mean_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SCV_monthly_mean_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SPM_monthly_mean_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SPM_monthly_mean_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_max_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_max_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_mean_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_mean_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_min_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_min_SD.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SWS_monthly_mean_Mean.tif',
+  'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SWS_monthly_mean_SD.tif',
+  ]
+
 export const metadata = {
   CHL_monthly_mean: {
     description:
@@ -96,8 +115,8 @@ export const metadata = {
     dataset: 'cmems_obs-oc_glo_bgc-plankton_my_l4-multi-4km_P1M',
     date: 'CHL_09-1997_05-2024',
     urls: [
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/CHL_monthly_mean_1997_2024_Mean.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/CHL_monthly_mean_1997_2024_SD.tif',
+      tiffsUrls[0], // CHL_monthly_mean_Mean
+      tiffsUrls[1], // CHL_monthly_mean_SD
     ],
     attributions: attributions.copernicus,
   },
@@ -114,16 +133,69 @@ export const metadata = {
     ],
     date: ['02/09/1981→31/12/2016', '02/01/2017→30/09/2022'],
     url: [
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_max_1985_2024_Mean.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_max_1985_2024_SD.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_mean_1985_2024_Mean.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_mean_1985_2024_SD.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_min_1985_2024_Mean.tif',
-      'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SST_monthly_min_1985_2024_SD.tif',
+      tiffsUrls[8], // SST_monthly_max_Mean
+      tiffsUrls[9], // SST_monthly_max_SD
+      tiffsUrls[10], // SST_monthly_mean_Mean
+      tiffsUrls[11], // SST_monthly_mean_SD
+      tiffsUrls[12], // SST_monthly_min_Mean
+      tiffsUrls[13], // SST_monthly_min_SD
     ],
     attributions: attributions.copernicus,
   },
+  SPM_monthly_mean: {
+    description: 'SPM suspended particulate matter: Mass concentration of suspended matter in sea water SPM [g/m3]',
+    shortTitle: 'Mass concentration of suspended matter in sea water SPM [g/m3]',
+    identifier: 'OCEANCOLOUR_GLO_BGC_L4_MY_009_104',
+    datasetDescription: 'Global Ocean Colour (Copernicus-GlobColour), Bio-Geo-Chemical, L4 (monthly and interpolated) from Satellite Observations (1997-ongoing)',
+    dataset: 'cmems_obs-oc_glo_bgc-transp_my_l4-multi-4km_P1M',
+    date: 'SPM_09-1997_05-2024',
+    urls: [
+      tiffsUrls[6], // SPM_monthly_mean_Mean
+      tiffsUrls[7], // SPM_monthly_mean_SD
+    ],
+    attributions: attributions.copernicus,
+  },
+  DHW_annual_max: {
+    description: 'DHW Degree Heating Week: The accumulation of thermal stress (i.e. temperature >1°C above the monthly maximal mean temperature) over the previous 12 weeks',
+    shortTitle: 'Degree Heating Week [°C-weeks]',
+    identifier: 'ct5km_dhw-max_v3.1',
+    datasetDescription: 'Year-to-date Annual Composites of 5km Satellite Coral Bleaching Heat Stress Products (Version 3.1)',
+    dataset: 'ct5km_dhw-max_v3.1',
+    date: '1985-2023',
+    urls: [
+      tiffsUrls[2], // DHW_annual_max_Mean
+      tiffsUrls[3], // DHW_annual_max_SD
+    ],
+    attributions: attributions.noaa,
+  },
+  SCV_monthly_mean: {
+    description: 'SCV sea current velocity calculated from NWV Northward sea water velocity and EWV Eastern sea water velocity',
+    shortTitle: 'Sea current velocity [m/s]',
+    identifier: 'GLOBAL_MULTIYEAR_PHY_001_030',
+    datasetDescription: 'Global Ocean Physics Reanalysis',
+    dataset: 'cmems_mod_glo_phy_my_0.083_P1D-m',
+    date: '01-1993_06-2021',
+    urls: [
+      tiffsUrls[4], // SCV_monthly_mean_Mean
+      tiffsUrls[5], // SCV_monthly_mean_SD
+    ],
+    attributions: attributions.copernicus,
+  },
+  SWS_monthly_mean: {
+    description: 'SWS Sea water salinity [10-3]',
+    shortTitle: 'Sea water salinity [10-3]',
+    identifier: 'GLOBAL_MULTIYEAR_PHY_001_030',
+    datasetDescription: 'Global Ocean Physics Reanalysis',
+    dataset: 'cmems_mod_glo_phy_my_0.083_P1D-m',
+    date: 'SWS_01-1993_06-2021',
+    urls: [
+      tiffsUrls[14], // SWS_monthly_mean_Mean
+      tiffsUrls[15], // SWS_monthly_mean_SD
+    ],
+    attributions: attributions.noaa,
+  },
 };
+
 
 export const sources: SourceInfo[] = [
   {
@@ -151,7 +223,7 @@ export const sources: SourceInfo[] = [
     ...legendVariables.SST,
     colorScale: SST_monthly_max_Mean,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_max_Mean.tif',
+    url: metadata.SST_monthly.url[0],
   },
   {
     type: SourceType.SD,
@@ -160,49 +232,49 @@ export const sources: SourceInfo[] = [
     ...legendVariables.SST,
     colorScale: SST_monthly_max_SD,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_max_SD.tif',
+    url: metadata.SST_monthly.url[1],
   },
   {
     type: SourceType.MEAN,
     name: 'SST_monthly_mean',
     colorScale: SST_monthly_mean_Mean,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_mean_Mean.tif',
+    url: metadata.SST_monthly.url[2],
   },
   {
     type: SourceType.SD,
     name: 'SST_monthly_mean',
     colorScale: SST_monthly_mean_SD,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_mean_SD.tif',
+    url: metadata.SST_monthly.url[3],
   },
   {
     type: SourceType.MEAN,
     name: 'SST_monthly_min',
     colorScale: SST_monthly_min_Mean,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_min_Mean.tif',
+    url: metadata.SST_monthly.url[4],
   },
   {
     type: SourceType.SD,
     name: 'SST_monthly_min',
     colorScale: SST_monthly_min_SD,
     attribution: attributions.copernicus,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/processed_data/SG_MANON/env_rasters_cut/SST_monthly_min_SD.tif',
+    url: metadata.SST_monthly.url[5],
   },
   {
     name: 'SPM_monthly_mean',
     attribution: attributions.copernicus,
     colorScale: SPM_monthly_mean_Mean,
     type: SourceType.MEAN,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SPM_monthly_mean_1997_2024_Mean.tif',
+    url: metadata.SPM_monthly_mean.urls[0],
   },
   {
     name: 'SPM_monthly_mean',
     attribution: attributions.copernicus,
     colorScale: SPM_monthly_mean_SD,
     type: SourceType.SD,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SPM_monthly_mean_1997_2024_SD.tif',
+    url: metadata.SPM_monthly_mean.urls[1],
   },
 
   {
@@ -210,14 +282,14 @@ export const sources: SourceInfo[] = [
     colorScale: DHW_annual_max_Mean,
     attribution: attributions.copernicus,
     type: SourceType.MEAN,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/DHW_annual_max_1985_2024_Mean.tif',
+    url: metadata.DHW_annual_max.urls[0],
   },
   {
     name: 'DHW_annual_max',
     colorScale: DHW_annual_max_SD,
     attribution: attributions.copernicus,
     type: SourceType.SD,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/DHW_annual_max_1985_2024_SD.tif',
+    url: metadata.DHW_annual_max.urls[1],
   },
 
   {
@@ -225,14 +297,14 @@ export const sources: SourceInfo[] = [
     colorScale: SCV_monthly_mean_Mean,
     attribution: attributions.copernicus,
     type: SourceType.MEAN,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SCV_monthly_mean_1993_2021_Mean.tif',
+    url: metadata.SCV_monthly_mean.urls[0],
   },
   {
     name: 'SCV_monthly_mean',
     colorScale: SCV_monthly_mean_SD,
     attribution: attributions.copernicus,
     type: SourceType.SD,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SCV_monthly_mean_1993_2021_SD.tif',
+    url: metadata.SCV_monthly_mean.urls[1],
   },
 
   {
@@ -240,14 +312,14 @@ export const sources: SourceInfo[] = [
     colorScale: SWS_monthly_mean_Mean,
     attribution: attributions.noaa,
     type: SourceType.MEAN,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SWS_monthly_mean_1993_2021_Mean.tif',
+    url: metadata.SWS_monthly_mean.urls[0],
   },
   {
     name: 'SWS_monthly_mean',
     colorScale: SWS_monthly_mean_SD,
     attribution: attributions.noaa,
     type: SourceType.SD,
-    url: 'https://enacit4r-cdn.epfl.ch/lgb-trsc/dev/ENV_RASTERS/SWS_monthly_mean_1993_2021_SD.tif',
+    url: metadata.SWS_monthly_mean.urls[1],
   },
 ];
 
