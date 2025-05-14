55f9bdf0e99bbf2f6ee958e86b5b1c9d948fe752
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-05-14 16:30:03 +0200

    feat(i18n): update community names to use translation keys and add multilingual support for community descriptions

--- frontend/src/pages/CommunityPage.vue
+++ frontend/src/pages/CommunityPage.vue
@@ -1,27 +1,26 @@
 <template>
   <q-page class="col items-center justify-center community-page">
-    <!-- <h1 class="red-bold">Community</h1> -->
     <p class="q-pa-md">
-      <a class="trsc-link" href="https://trsc.org" target="_blank"
-        >The Transnational Red Sea Center</a
-      >
-      relies on many collaborators from the Red Sea region and Gulf of Aden
+      <a class="trsc-link" href="https://trsc.org" target="_blank">
+        {{ t('community.header') }}
+      </a>
+      {{ t('community.description') }}
     </p>
     <div class="logo-row">
       <div
         v-for="community in communities"
-        :key="community.name"
+        :key="community.nameKey"
         class="logo-item"
       >
         <a :href="community.url" target="_blank" class="logo-item-link">
           <q-img
             :src="community.logo"
-            :alt="community.name"
+            :alt="t(community.nameKey)"
             fit="contain"
             style="height: 150px; width: 150px"
           >
           </q-img>
-          <p>{{ community.name }}</p>
+          <p>{{ t(community.nameKey) }}</p>
         </a>
       </div>
     </div>
@@ -29,8 +28,62 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import communities from 'assets/communities';
+
+const { t } = useI18n();
 </script>
+
+<i18n lang="yaml">
+en:
+  community:
+    header: "The Transnational Red Sea Center"
+    description: "relies on many collaborators from the Red Sea region and Gulf of Aden"
+  communities:
+    aqabaSpecialEconomicZoneAuthority: "Aqaba Special Economic Zone Authority, Jordan"
+    marineScienceStationOfAqaba: "Marine Science Station of Aqaba, Jordan"
+    aqabaMarineReserve: "Aqaba Marine Reserve, Jordan"
+    redSeaUniversityOfPortSudan: "Red Sea University of Port-Sudan, Sudan"
+    ministryOfMaritimeResourcesOfMassawa: "Ministry of Maritime Resources of Massawa, Eritrea"
+    ministereEnvironnementDjibouti: "Ministère de l'Environnement et du Développement Durable de Djibouti"
+    universiteDeDjibouti: "Université de Djibouti"
+    centreEtudeRechercheDjibouti: "Centre d'étude et de recherche de Djibouti"
+    universityOfKonstanz: "University of Konstanz, Germany"
+    ecolePolytechniqueFederealeZurich: "Swiss Federal Institute of Technology Zurich, Switzerland"
+    ecolePolytechniqueFederealeLausanne: "Swiss Federal Institute of Technology Lausanne, Switzerland"
+fr:
+  community:
+    header: "Le Centre Transnational de la Mer Rouge"
+    description: "compte sur de nombreux collaborateurs de la région de la Mer Rouge et du Golfe d'Aden"
+  communities:
+    aqabaSpecialEconomicZoneAuthority: "Aqaba Special Economic Zone Authority, Jordan"
+    marineScienceStationOfAqaba: "Marine Science Station of Aqaba, Jordan"
+    aqabaMarineReserve: "Aqaba Marine Reserve, Jordan"
+    redSeaUniversityOfPortSudan: "Red Sea University of Port-Sudan, Sudan"
+    ministryOfMaritimeResourcesOfMassawa: "Ministère des Ressources Maritimes de Massawa, Eritrea"
+    ministereEnvironnementDjibouti: "Ministère de l'Environnement et du Développement Durable de Djibouti"
+    universiteDeDjibouti: "Université de Djibouti"
+    centreEtudeRechercheDjibouti: "Centre d'étude et de recherche de Djibouti"
+    universityOfKonstanz: "University of Konstanz, Germany"
+    ecolePolytechniqueFederealeZurich: "École polytechnique fédérale de Zurich, Switzerland"
+    ecolePolytechniqueFederealeLausanne: "École polytechnique fédérale de Lausanne, Switzerland"
+ar:
+  community:
+    header: "المركز العابر للحدود لبحر الأحمر"
+    description: "يعتمد على العديد من المتعاونين من منطقة البحر الأحمر وخليج عدن"
+  communities:
+    aqabaSpecialEconomicZoneAuthority: "Aqaba Special Economic Zone Authority, Jordan"
+    marineScienceStationOfAqaba: "Marine Science Station of Aqaba, Jordan"
+    aqabaMarineReserve: "Aqaba Marine Reserve, Jordan"
+    redSeaUniversityOfPortSudan: "Red Sea University of Port-Sudan, Sudan"
+    ministryOfMaritimeResourcesOfMassawa: "Ministry of Maritime Resources of Massawa, Eritrea"
+    ministereEnvironnementDjibouti: "Ministère de l'Environnement et du Développement Durable de Djibouti"
+    universiteDeDjibouti: "Université de Djibouti"
+    centreEtudeRechercheDjibouti: "Centre d'étude et de recherche de Djibouti"
+    universityOfKonstanz: "University of Konstanz, Germany"
+    ecolePolytechniqueFederealeZurich: "École polytechnique fédérale de Zurich, Switzerland"
+    ecolePolytechniqueFederealeLausanne: "École polytechnique fédérale de Lausanne, Switzerland"
+</i18n>
 
 <style scoped>
 .trsc-link {
